84eeef0dcf70ac8d23ae3bce30c4bbc77c7273ca
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-20 13:29:02 +0200

    Initial Commit

---
+++ StackViewExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackViewExercise
+//
+//  Created by Usama Eltmsah on 10/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
